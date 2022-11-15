a764fced9dfda3f11eaea245e27c0e982fd7c1fc
Crowdbotics <engineering@crowdbotics.com>
2022-11-15 11:33:20 +0000

    Update store.

--- store/testwdewdede/api.js
+++ store/testwdewdede/api.js
@@ -1,11 +1,14 @@
 import axios from "axios"
 import {
-  TESTWDEWDEDE_USERNAME,
-  TESTWDEWDEDE_PASSWORD
+  TESTWDEWDESAWDASDFGHSDEWDE_USERNAME,
+  TESTWDEWDESAWDASDFGHSDEWDE_PASSWORD
 } from "react-native-dotenv"
 const testwdewdede = axios.create({
   baseURL: "https://cbtcms.herokuapp.com/runs/2927/?",
-  auth: { username: TESTWDEWDEDE_USERNAME, password: TESTWDEWDEDE_PASSWORD },
+  auth: {
+    username: TESTWDEWDESAWDASDFGHSDEWDE_USERNAME,
+    password: TESTWDEWDESAWDASDFGHSDEWDE_PASSWORD
+  },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 export const apiService = {}
