717990a0d03d2dcad2222f3542aa40709fb0d487
Crowdbotics <engineering@crowdbotics.com>
2022-11-15 11:53:03 +0000

    Update store.

--- store/testapiconnectorsputeeee/api.js
+++ store/testapiconnectorsputeeee/api.js
@@ -1,5 +1,4 @@
 import axios from "axios"
-import {} from "react-native-dotenv"
 const testapiconnectorsputeeee = axios.create({
   baseURL: "https://cbtcms.herokuapp.com/runs/2927/?",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
